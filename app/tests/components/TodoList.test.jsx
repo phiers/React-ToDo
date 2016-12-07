e1f8c791119c2cf20ec193db5598704e1019c34b
Paul Hiers Jr <paulhiers@yahoo.com>
2016-12-07 10:18:06 -0500

    fix tests

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -6,10 +6,27 @@ import $ from 'jQuery';
 import TestUtils from 'react-addons-test-utils';
 
 import TodoList from 'TodoList';
+import Todo from 'Todo';
 /* eslint-enable */
 /* eslint-disable no-undef */
 describe('TodoList', () => {
   it('should exist', () => {
     expect(TodoList).toExist();
+  });
+  it('should render one todo component for each todo item', () => {
+    const todos = [
+      {
+        id: 1,
+        text: 'item 1',
+      },
+      {
+        id: 2,
+        text: 'item 2',
+      },
+    ];
+    const todoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
+    const todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+    expect(todosComponents.length).toBe(todos.length);
   });
 });
